a072e72727350b161828d2327942db92b5dec2ad
PatrickFeeney <patrickfeeney1225@gmail.com>
2020-11-20 12:08:39 -0500

    Updated Resp and Objective Outline

--- dp_mix/models.py
+++ dp_mix/models.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Ellipse
 import numpy as np
-from scipy.special import softmax
+from scipy.special import softmax, digamma
 from scipy.stats import multivariate_normal as mv_norm
 
 
@@ -26,20 +26,7 @@ class DPMix:
 
     def _update(self):
         # local step
-        # C
-        # approximation of prior mean from Eq. 2.51
-        # TODO use actual distribution instead of approximation
-        cluster_means = self.obs_mean()
-        log_cluster_weight = np.asarray([mv_norm.logpdf(self.data, cluster_means[i])
-                                         for i in range(self.truncation)]).T
-        # from Eq. 3.17-18
-        log_u_hat = np.log(self.allo_param[:, 1] / np.sum(self.allo_param, axis=1))
-        # W
-        log_post_weight = log_cluster_weight + \
-            np.asarray([[log_u_hat[i] + np.sum(1 - log_u_hat[:i])
-                        for i in range(self.truncation)]])
-        # r hat
-        resp = softmax(log_post_weight, axis=0)
+        resp = self.responsibility()
 
         # summary step
         # S
@@ -75,3 +62,31 @@ class DPMix:
 
     def obs_mean(self):
         return self.obs_count / self.obs_shape[:, np.newaxis]
+
+    def responsibility(self):
+        # C
+        # approximation of prior mean from Eq. 2.51
+        # TODO use actual distribution instead of approximation
+        cluster_means = self.obs_mean()
+        log_cluster_weight = np.asarray([mv_norm.logpdf(self.data, cluster_means[i])
+                                         for i in range(self.truncation)]).T
+        # expectations over Dirichlet
+        log_u = digamma(self.allo_param[:, 1]) - digamma(np.sum(self.allo_param, axis=1))
+        log_1_minus_u = digamma(self.allo_param[:, 0]) - digamma(np.sum(self.allo_param, axis=1))
+        # W
+        log_post_weight = log_cluster_weight + \
+            np.asarray([[log_u[i] + np.sum(log_1_minus_u[:i])
+                        for i in range(self.truncation)]])
+        # r hat
+        return softmax(log_post_weight, axis=0)
+
+    # TODO implement objective function
+    def objective(self):
+        resp = self.responsibility()
+        # data loss
+        data_loss = 0
+        # entropy loss
+        entropy_loss = -1 * np.sum(resp * np.log(resp))
+        # DP allocation loss
+        dp_alloc_loss = 0
+        return data_loss + entropy_loss + dp_alloc_loss
